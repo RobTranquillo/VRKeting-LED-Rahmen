503496e99ae2a87ddb750a7a1e6fa777527939c4
Rob <rw@boogiedev.net>
2022-10-02 21:23:57 +0200

    html from seperatte file

--- webserver.py
+++ webserver.py
@@ -8,18 +8,11 @@ except:
 from machine import Pin
 led = Pin(2, Pin.OUT)
 
-myText = "FooBaZ"
-
 def web_page():
-
-  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
-  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
-  <p>GPIO state: <strong>""" + myText + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
-  return html
+  text_file = open("html/index.html", "r")
+  index_html = text_file.read()
+  text_file.close()
+  return index_html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
