6ebab14a12aa59a471cec83dcfe6075a0d7265e0
Rob <rw@boogiedev.net>
2022-10-09 12:39:58 +0200

    delete unused stuff

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ import time
 #custom libs
 import leds
 
-# leds.OnOffSingle()
-# leds.AllBlue()
-# while True:
-#     leds.PixelWithTail()
-#     leds.AllRandom()
-#     time.sleep(4)
-
-
 # run single intro ligths
 leds.run("flash")
 
